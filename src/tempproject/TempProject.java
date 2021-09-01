c7dc2bf1b2e276db60cac5239ab801aa47fe2e13
ptp-phamphong <74300269+ptp-phamphong@users.noreply.github.com>
2021-09-01 08:24:27 +0700

    Initialize 1 cái project mới toanh, pull về mà liếm thôi!!!!!

---
+++ src/tempproject/TempProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tempproject;
+
+/**
+ *
+ * @author HP Omen
+ */
+public class TempProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
